a2fc80d8bd0fa781aadbb33d6dcead56f9300b12
gpr2109 <ragavangp12b320.2122@gmail.com>
2021-10-04 20:22:24 +0530

    Updated

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -12,6 +12,9 @@ import XMonad.Layout.BinarySpacePartition
 import XMonad.Layout.SimplestFloat
 import XMonad.Layout.NoBorders
 
+import XMonad.Actions.FloatSnap
+import XMonad.Actions.FloatKeys
+
 import XMonad.Util.SpawnOnce
 import XMonad.Util.Run
 import XMonad.Util.EZConfig
@@ -20,15 +23,13 @@ import qualified XMonad.StackSet as W
 
 myModMask = mod4Mask
 
-xmobarTitleColor = "#22CCDD"
-
--- Color of current workspace in xmobar.
-xmobarCurrentWorkspaceColor = "#CEFFAC" 
-
-myBorderWidth = 2
+myBorderWidth = 0
 
 myNormalBorderColor = "#acb0d0"
 myFocusedBorderColor = "#7aa2f7"
+
+myManageHook = composeAll
+    [ className =? "vlc"        --> doFloat ] 
 
 myKeys :: [(String, X ())]
 myKeys = 
@@ -41,22 +42,32 @@ myKeys =
         , ("<Print>", spawn "scrot")
         , ("<XF86MonBrightnessUp>", spawn "xbacklight -inc 5")
         , ("<XF86MonBrightnessDown>", spawn "xbacklight -dec 5")
+        , ("M-d", withFocused (keysMoveWindowTo (683,384) (0.5,0.5)))
+
+        , ("M-M1-<Up>", withFocused (keysMoveWindow (0,-5)))
+        , ("M-M1-<Right>", withFocused (keysMoveWindow (5,0)))
+        , ("M-M1-<Down>", withFocused (keysMoveWindow (0,5)))
+        , ("M-M1-<Left>", withFocused (keysMoveWindow (-5,0)))
+
+        , ("M-S-<Up>", withFocused (keysResizeWindow (0,10) (0.5,0.5)))
+        , ("M-S-<Right>", withFocused (keysResizeWindow (10,0) (0.5,0.5)))
+        , ("M-S-<Down>", withFocused (keysResizeWindow (0,-10) (0.5,0.5)))
+        , ("M-S-<Left>", withFocused (keysResizeWindow (-10,0) (0.5,0.5)))
         ]
 
 myStartupHook = do
-    spawnOnce "bash ~/.fehbg"
-    spawnOnce "bash ~/pipewire.sh"
-    spawnOnce "xbacklight -set 10"
-    spawnOnce "lxsession"
+    spawnOnce "hsetroot -solid '#7aa2f7' &"
+    spawnOnce "bash ~/pipewire.sh &"
+    spawnOnce "picom &"
+    spawnOnce "xbacklight -set 10 &" 
 
 tall =  spacingRaw True (Border 0 0 0 0) True (Border 5 5 5 5) True $ gaps [(U,29), (R,10), (L,10), (D,10)] $ avoidStruts $ Tall 1 (3/100) (50/100)
 
 bsp =  spacingRaw True (Border 0 0 0 0) True (Border 5 5 5 5) True $ gaps [(U,29), (R,10), (L,10), (D,10)] $ emptyBSP
 
-myLayoutHook = tall ||| bsp ||| noBorders Full ||| simplestFloat 
+myLayoutHook = tall ||| noBorders tall |||  bsp ||| noBorders Full ||| simplestFloat 
 
-main = do 
-    -- xmproc <- spawnPipe "dbus-launch xmobar ~/.config/xmobar/xmobar.hs"
+main = do  
     xmproc <- spawnPipe "dbus-launch xmobar -d"
     xmonad $ docks def {
                   startupHook        = myStartupHook
@@ -64,13 +75,15 @@ main = do
                 , borderWidth        = myBorderWidth
                 , normalBorderColor  = myNormalBorderColor
                 , focusedBorderColor = myFocusedBorderColor
-                , layoutHook         = myLayoutHook 
-                , logHook = dynamicLogWithPP $
+                , layoutHook         = myLayoutHook
+                , manageHook         = myManageHook 
+                , logHook            = dynamicLogWithPP $
                     xmobarPP {
                               ppOutput = hPutStrLn xmproc
-                            -- , ppTitle = xmobarColor "#FFFFFF" "" . shorten 100
-                            -- , ppCurrent = xmobarColor "#000000" ""
-                            -- , ppSep = "   "
+                             , ppTitle = xmobarColor "#FFFFFF" "" . shorten 100
+                             , ppCurrent = xmobarColor "#7aa2f7" ""
+                             , ppSep = "   "
+                             , ppOrder  = \(ws : l : _ : _ ) -> [ws,l]
                             }
                 } `additionalKeysP` myKeys
                
--- xmobar/xmobar.hs
+++ xmobar/xmobar.hs
@@ -1,14 +1,14 @@
-Config { font = "xft:DejaVu Sans:style=Regular:size=8:antialias=true:hinting=true" 
+Config { font = "xft:Ubuntu Mono:style=Regular:size=10:antialias=true:hinting=true" 
        , additionalFonts = [ "xft:Font Awesome 5 Free Solid:pixelsize=8" 
                            , "xft:Font Awesome 5 Brands:pixelsize=8" ]
        , borderColor = "black"
        , border = TopB
        , bgColor = "#1a1b26"
-       , fgColor = "#a9b1d6"
+       , fgColor = "#ffffff"
        , alpha = 255
        , position = Top -- Static { xpos = 0, ypos = 0, width = 1346, height = 20 },
-       , textOffset = -1
-       , iconOffset = -1
+       , textOffset = -3
+       , iconOffset = -2
        , lowerOnStart = True
        , pickBroadest = False
        , persistent = False
@@ -16,16 +16,7 @@ Config { font = "xft:DejaVu Sans:style=Regular:size=8:antialias=true:hinting=tru
        , iconRoot = ".xmonad/xpm/"
        , allDesktops = True
        , overrideRedirect = True
-       , commands = [ Run Weather "EGPF" ["-t","<station>: <tempC>C",
-                                          "-L","18","-H","25",
-                                          "--normal","green",
-                                          "--high","red",
-                                          "--low","lightblue"] 36000
-                    , Run Network "eth0" ["-L","0","-H","32",
-                                          "--normal","green","--high","red"] 10
-                    , Run Network "eth1" ["-L","0","-H","32",
-                                          "--normal","green","--high","red"] 10
-                    , Run Cpu ["-L","3","-H","50",
+       , commands = [ Run Cpu ["-L","3","-H","50",
                                "--normal","green","--high","red"] 10
                     , Run Memory ["-t","Mem: <usedratio>%"] 10
                     , Run Swap [] 10
@@ -36,7 +27,7 @@ Config { font = "xft:DejaVu Sans:style=Regular:size=8:antialias=true:hinting=tru
                     ]
        , sepChar = "%"
        , alignSep = "}{"
-       , template = "<icon=haskell_20.xpm/>  %StdinReader% }\
-                    \ {%cpu% | %memory% * %swap% | <fc=#ee9a00>%date%</fc>"
+       , template = " <icon=haskell_20.xpm/> %StdinReader% }\
+                    \ {<fc=#f7768e>%cpu%</fc> | <fc=#7aa2f7>%memory% * %swap%</fc> | <fc=#9a7ecc>%date%</fc> "
 }
 
