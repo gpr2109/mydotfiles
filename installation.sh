06a9724c5a8a42cd5ca37e9cd5ef89d26ba306ed
gpr2109 <ragavan12b320.2122@gmail.com>
2021-10-10 13:52:21 +0530

    Update with my Dmenu

--- installation.sh
+++ installation.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
-cp -r .xmonad ~/
-cp -r .xmobar ~/.config/
-cp -r macchina ~/
-cp .Xresources ~/
+cp -r .xmonad ~/ &
+cp -r .xmobar ~/.config/ &
+cp -r macchina ~/ &
+cp .Xresources ~/ &
+
+sudo pacman -S --needed git hsetroot xmonad xmobar xmonad-contrib vim &
+
+sudo cp xorg.conf.d /etc/X11/ &
+
