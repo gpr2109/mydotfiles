79d321499eebba5200454bf4aa9424e62ba1bf3f
gpr2109 <ragavan12b320.2122@gmail.com>
2021-10-10 13:56:48 +0530

    Updated my installtion.sh

--- installation.sh
+++ installation.sh
@@ -5,7 +5,11 @@ cp -r .xmobar ~/.config/ &
 cp -r macchina ~/ &
 cp .Xresources ~/ &
 
-sudo pacman -S --needed git hsetroot xmonad xmobar xmonad-contrib vim &
+sudo pacman -S --needed hsetroot ttf-ubuntu-mono xmonad xmobar xmonad-contrib vim &
 
 sudo cp xorg.conf.d /etc/X11/ &
+
+cd dmenu-patched ; sudo make install &
+
+
 
